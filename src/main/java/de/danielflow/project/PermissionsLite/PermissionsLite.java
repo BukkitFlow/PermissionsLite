fcfc2d6dd7bdac0e3071234db885c7584d0c96e3
BuildTools <unconfigured@null.spigotmc.org>
2018-03-03 23:13:29 +0100

    first commit

---
+++ src/main/java/de/danielflow/project/PermissionsLite/PermissionsLite.java
@@ -0,0 +1,19 @@
+package de.danielflow.project.PermissionsLite;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PermissionsLite extends JavaPlugin{
+
+	@Override
+	public void onDisable() {
+		
+	}
+
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	
+
+}
